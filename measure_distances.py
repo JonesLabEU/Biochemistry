d6e9d3e6bc9e5d8b27b7468a9bb60de934f1f24a
JusSva <119876695+JusSva@users.noreply.github.com>
2024-06-26 17:55:06 +0300

    Add files via upload
    The Cas9 and Cas12a notebooks are the ones I used for data analysis. Measure_distances.ipynb contains all of the functions that were used. I would copy-paste the functions from .ipynb file into measure_distances.py and then run that file in PyMol.

---
+++ measure_distances.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[5]:
+
+
+from pymol import cmd
+
+def measure_distances():
+    # Replace 'nuclease' with the name of the nuclease object in PyMOL
+    nuclease_object = 'chain A'
+    
+    # Replace 'dna' with the name of the DNA object in PyMOL
+    dna_object = 'chain C'
+    
+    # Replace 'protein' with the name of the protein object in PyMOL
+    protein_selection = f'sele and {nuclease_object}'
+    
+    # Create a dictionary to store the minimum distances for each DNA atom
+    distances = {}
+    
+    # Get the list of atoms in the DNA object
+    dna_atoms = cmd.get_model(dna_object).atom
+    
+    for dna_atom in dna_atoms:
+        min_dist = float('inf')  # Initialize minimum distance to a large value
+        for protein_atom in cmd.get_model(protein_selection).atom:
+            # Calculate the distance between the DNA atom and the protein atom
+            dist = cmd.distance(f'dist_{dna_atom.index}_{protein_atom.index}', dna_atom.index, f'{nuclease_object} and index {protein_atom.index}')
+            min_dist = min(min_dist, dist)  # Update minimum distance
+        
+        # Store the minimum distance for the current DNA atom
+        distances[dna_atom] = min_dist
+    
+    print("Distance between DNA nucleotides and closest protein atom:")
+    for dna_atom, dist in distances.items():
+        # Get the DNA residue number and atom name
+        residue_number = dna_atom.resi_number
+        atom_name = dna_atom.name
+        
+        print(f"DNA residue {residue_number} {atom_name}: {dist:.3f} Å")
+
+measure_distances()
+cmd.extend("measure_distances",measure_distances)
+
